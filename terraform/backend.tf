f3d832e915ea6a99e608632e699b3eb7ff920d9d
Marwo Muhamed <marwa@Marwos-MacBook-Pro.local>
2025-12-06 14:48:55 +0000

    Use DynamoDB for Terraform state locking

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "memos-tf-state-9372jk"
-    key          = "memos/terraform.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
-    use_lockfile = true
+    bucket         = "memos-tf-state-9372jk"
+    key            = "memos/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "memos-tf-locks"
   }
 }
